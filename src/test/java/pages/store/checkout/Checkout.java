c18f48557f0275e86b8ccc3f1fa575b6f62ce9f2
maxim <laba1203@gmail.com>
2018-04-22 04:21:11 +0300

    Steps 7-14

--- src/test/java/pages/store/checkout/Checkout.java
+++ src/test/java/pages/store/checkout/Checkout.java
@@ -13,33 +13,55 @@ public class Checkout extends AbstractElementsContainer{
     private static final By orderSummaryRowLctr = By.xpath("//td[@class = 'item']");
     private static final By removeButtonLctr = By.cssSelector("button[name = 'remove_cart_item']");
 
+    private static final By firstShortcutLctr = By.xpath("//ul[@class = 'shortcuts']/li[1]/a");
+
+    private static final By firstRemoveBtnLctr = By.xpath("//ul[@class = 'items']/li[1]//button[@name='remove_cart_item']");
+
     private List<WebElement> orderSummaryRows;
     private List<WebElement> removeButtons;
+
 
     public Checkout(){
         setElements();
     }
 
+
     public Checkout removeAllOrders(){
         while (orderSummaryRows.size() > 0){
-            removeVisibleProduct();
+//            removeVisibleProduct();
+            removeFirstOrder();
         }
         return new Checkout();
     }
 
-    private void removeVisibleProduct(){
+    private void removeFirstOrder(){
         int initialOrdersSize = orderSummaryRows.size();
-        for (WebElement el:
-             removeButtons) {
-            if(el.isDisplayed()){
-                el.click();
-                DriverFactory.getWait(5, 500)
-                        .until(ExpectedConditions.numberOfElementsToBe(orderSummaryRowLctr, initialOrdersSize));
-                break;
-            }
-        }
+        WebElement firstShortcut = driver.findElement(firstShortcutLctr);
+        firstShortcut.click();
+        WebElement deleteButton = driver.findElement(firstRemoveBtnLctr);
+        deleteButton.click();
+
+        DriverFactory.getWait(5, 500)
+                        .until(ExpectedConditions.numberOfElementsToBe(orderSummaryRowLctr, initialOrdersSize - 1));
         setElements();
     }
+
+
+
+//    private void removeVisibleProduct(){
+//        setElements();
+//        int initialOrdersSize = orderSummaryRows.size();
+//        for (WebElement el:
+//             removeButtons) {
+//            if(el.isDisplayed()){
+//                el.click();
+//                DriverFactory.getWait(5, 500)
+//                        .until(ExpectedConditions.numberOfElementsToBe(orderSummaryRowLctr, initialOrdersSize));
+//                break;
+//            }
+//        }
+//        setElements();
+//    }
 
     private void setElements(){
         orderSummaryRows = driver.findElements(orderSummaryRowLctr);
